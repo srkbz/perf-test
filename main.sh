85a60ab2a166c4f93f367d3fa8df0f2cdf27bd00
Carlos Fdez. Llamas <hello@sirikon.me>
2020-06-24 01:53:55 +0200

    Multicore

--- main.sh
+++ main.sh
@@ -12,7 +12,8 @@ function main {
 
     ensure-deps
 
-    cpu-bench
+    cpu-single-core-bench
+    cpu-all-cores-bench
     memory-bench
     fileio-sequential-read-bench
     fileio-sequential-write-bench
@@ -29,27 +30,26 @@ function ensure-deps {
     printf "\n"
 }
 
-function cpu-bench {
-    pre-bench "CPU"
-    sysbench cpu run --cpu-max-prime=50000 \
-        | read-values \
-            "events per second" \
-            "total time" \
-            "total number of events" \
-            "min" \
-            "avg" \
-            "max" \
-            "95th percentile" \
-            "sum" \
-            "events \(avg/stddev\)" \
-            "execution time \(avg/stddev\)" \
-        > "$PERF_RESULTS/cpu-results.tsv"
+function cpu-single-core-bench {
+    pre-bench "CPU Single Core"
+    sysbench cpu run --threads=1 --cpu-max-prime=50000 \
+        | read-cpu-values \
+        > "$PERF_RESULTS/cpu-single-core-results.tsv"
+    post-bench
+}
+
+function cpu-all-cores-bench {
+    pre-bench "CPU All Cores"
+    printf "threads\t%s\n" "$(nproc)" > "$PERF_RESULTS/cpu-all-cores-results.tsv"
+    sysbench cpu run --threads=$(nproc) --cpu-max-prime=50000 \
+        | read-cpu-values \
+        >> "$PERF_RESULTS/cpu-all-cores-results.tsv"
     post-bench
 }
 
 function memory-bench {
     pre-bench "Memory"
-    sysbench memory run \
+    sysbench memory run --threads=$(nproc) \
         | read-values \
             "total time" \
             "total number of events" \
@@ -67,7 +67,7 @@ function memory-bench {
 function fileio-sequential-read-bench {
     pre-bench "FileIO Sequential Read"
     sysbench fileio prepare --file-test-mode=seqrd > /dev/null
-    sysbench fileio run --file-test-mode=seqrd \
+    sysbench fileio run --threads=$(nproc) --file-test-mode=seqrd \
         | read-fileio-values \
         > "$PERF_RESULTS/fileio-sequential-read-results.tsv"
     post-bench
@@ -75,7 +75,7 @@ function fileio-sequential-read-bench {
 
 function fileio-sequential-write-bench {
     pre-bench "FileIO Sequential Write"
-    sysbench fileio run --file-test-mode=seqwr \
+    sysbench fileio run --threads=$(nproc) --file-test-mode=seqwr \
         | read-fileio-values \
         > "$PERF_RESULTS/fileio-sequential-write-results.tsv"
     post-bench
@@ -84,7 +84,7 @@ function fileio-sequential-write-bench {
 function fileio-sequential-rewrite-bench {
     pre-bench "FileIO Sequential Rewrite"
     sysbench fileio prepare --file-test-mode=seqrewr > /dev/null
-    sysbench fileio run --file-test-mode=seqrewr \
+    sysbench fileio run --threads=$(nproc) --file-test-mode=seqrewr \
         | read-fileio-values \
         > "$PERF_RESULTS/fileio-sequential-rewrite-results.tsv"
     post-bench
@@ -93,10 +93,24 @@ function fileio-sequential-rewrite-bench {
 function fileio-random-read-write-bench {
     pre-bench "FileIO Random Read/Write"
     sysbench fileio prepare --file-test-mode=rndrw > /dev/null
-    sysbench fileio run --file-test-mode=rndrw \
+    sysbench fileio run --threads=$(nproc) --file-test-mode=rndrw \
         | read-fileio-values \
         > "$PERF_RESULTS/fileio-random-read-write-results.tsv"
     post-bench
+}
+
+function read-cpu-values {
+    read-values \
+        "events per second" \
+        "total time" \
+        "total number of events" \
+        "min" \
+        "avg" \
+        "max" \
+        "95th percentile" \
+        "sum" \
+        "events \(avg/stddev\)" \
+        "execution time \(avg/stddev\)"
 }
 
 function read-fileio-values {
@@ -140,6 +154,7 @@ function pre-bench {
     cd $PERF_WORKDIR
     rm -rf ./*
     printf ":: Running bench: ${1}..."
+    sleep 10
 }
 
 function post-bench {
